115ae5397b289bf6e6a77f294d829a63d4ad3efc
cagri32 <43071105+cagri32@users.noreply.github.com>
2021-05-19 17:11:30 -0400

    Comments added to clarify

--- RateMyProf.py
+++ RateMyProf.py
@@ -8,28 +8,35 @@ from selenium.webdriver.chrome.options import Options
 from os import path
 from selenium.webdriver.common.by import By
 
-path_to_extension = r'C:\Users\YOUR_USER_NAME\Desktop\1.9.0_0'
 
+# To block the ads in the website, I install adblocker every time I run the code.
+# I will find a better, efficient way to handle this
+path_to_extension = r'C:\Users\YOUR_USER_NAME\Desktop\1.9.0_0'
 chop = webdriver.ChromeOptions()
 chop.add_extension(path.abspath('C:\\Users\\Cagri\\Desktop\\Python\\gighmmpiobklfepjocnamgkkbiglidom-4.33.0-Crx4Chrome.com.crx'))
 driver = webdriver.Chrome(options = chop)
 
 #driver = webdriver.Chrome('./chromedriver')
 
+# Go to the all York Professors page, wait 1 second and close the pop-up
 driver.get("https://www.ratemyprofessors.com/search/teachers?query=*&sid=1495")
 sleep(1)
 driver.find_element_by_xpath("//*[contains(text(), 'Close')]").click()
 
+# Profs with ratings over 4 has this class name
 NumRatingOver4 = 'div[@class="CardNumRating__CardNumRatingNumber-sc-17t4b9u-2 kMhQxZ"]'
 
+# Click Show More as many times as stated below
 #while driver.find_element_by_xpath("//button[contains(text(), 'Show More')]"):
 for i in range(10):
     driver.find_element_by_xpath("//button[contains(text(), 'Show More')]").click()
     sleep(0.4)
 
+# Get all ratings over 4
 Ratings = driver.find_elements_by_xpath('//%s'%NumRatingOver4)
 #Ratings = driver.find_elements_by_xpath('//div[@class="CardNumRating__CardNumRatingNumber-sc-17t4b9u-2 kMhQxZ"]/div[@class="CardName__StyledCardName-sc-1gyrgim-0 cJdVEK"]')
-#print(Ratings)
+
+# I am printing the found elements for now. They will be used differently in the future
 for x in range (len(Ratings)):
     rating = Ratings[x]
     numOfRatings = rating.find_element_by_xpath("./following-sibling::div")
@@ -39,11 +46,12 @@ for x in range (len(Ratings)):
     name = allInfo.find_element_by_xpath("./div[contains(@class, 'CardName__StyledCardName')]")
     department = name.find_element_by_xpath("./following-sibling::div/div[contains(@class, 'CardSchool__Department')]")
     university = name.find_element_by_xpath("./following-sibling::div/div[contains(@class, 'CardSchool__School')]")
+
+    # Other information to be obtained
     #takeAgain = allInfo.find_element_by_xpath("./div[contains(@class, 'CardFeedback__CardFeedbackNumber-lq6nix-2 hroXqf')]").text
     #difficulty = allInfo.find_element_by_xpath("./div[contains(@class, 'CardSchool__Department')]").text
     
-
-    #print(parent.find_element_by_xpath("./following-sibling::div/div[contains(@class, 'CardName__StyledCardName')]").text)
+    # Print the obtained info in a format
     print("Professor:\t\t"+name.text)
     print("Rating:\t\t\t"+Ratings[x].text)
     print("Number of Ratings is:\t"+numOfRatings.text)
