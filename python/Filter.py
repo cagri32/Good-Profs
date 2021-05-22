37921a960f9d7d46b19c91da480c5effc1f4ae9e
cagri32 <43071105+cagri32@users.noreply.github.com>
2021-05-22 00:54:04 -0400

    Script can be run with school name and department inputs now. 2-) Output is prettyprinted

--- python/Filter.py
+++ python/Filter.py
@@ -1,7 +1,26 @@
 import json
+import argparse
+
+# Following piece of code is utilized to receive arguments to run the script with
+parser=argparse.ArgumentParser()
+parser.add_argument('--school', help='Enter the school name you want to pull data from eg. York University - Keele')
+parser.add_argument('--department', help='Enter the department you want to see the professors of eg. Computer Science, Psychology')
+argms=parser.parse_args()
+# pass parameters as --bar=bar-val and access them using argms.bar
+
+# If school name passed, use it. Otherwise, use YorkUni2 as default
+if (argms.school != None): 
+    school = argms.school
+else:
+    school = "YorkUni2"
+# If department passed, use it. Otherwise, use Computer Science as default
+if (argms.department != None): 
+    department = argms.department
+else:
+    department = "Computer Science"
 
 # Load the list of all profs
-with open('YorkUni2.json') as f:
+with open('./../json/%s.json' %school) as f:
   data = json.load(f)
   
 # Create an empty dict to fill with filtered data
@@ -9,17 +28,17 @@ Profs = {}
 
 # Iterate through input json file
 for key in data:
-  if (data[key]['Department'] == 'Psychology'):
+  if (data[key]['Department'] == department):
       Profs[key] = data[key]
 
       # Keep these for testing purposes
       #print(data[key])
       #print(json.dumps(data[key], indent=4))
 
+depOutput = ("./../json/%s-%s.json" %(school, department))
 # Dump the dictionary to a json file
-with open('Psychology.json', 'w') as Out:
-    json.dump(Profs, Out)
-
+with open(depOutput, 'w') as Out:
+    json.dump(Profs, Out, indent=4)
 
 
 # outputfile = open("CompSci.json", "w")
